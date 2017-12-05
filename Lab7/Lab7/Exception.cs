adebc4655d2272da1d58477e35767ed33d8ae78c
Andrei Chayeuski <andreichayeuski@gmail.com>
2017-12-05 22:29:08 +0300

    Add files via upload

---
+++ Lab7/Lab7/Exception.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    public class NullException : Exception
+    {
+        public NullException(string message) : base(message)
+        {
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("NullException: " + this.Message);
+        }
+    }
+
+    public class EmptyException : Exception
+    {
+
+        public EmptyException(string message) : base(message)
+        {
+
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("EmptyException: " + this.Message);
+        }
+    }
+}
