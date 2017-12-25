3bcff3ba3be415b5d9ca3a8c2c1e72a9981e69a6
Andrei Chayeuski <andreichayeuski@gmail.com>
2017-12-25 15:16:42 +0300

    Add files via upload

---
+++ Lab12/Lab12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reflector reflector = new Reflector("Lab3.Book");
+            reflector.AboutClass();
+            reflector.PublicMethods();
+            reflector.Fields();
+            reflector.Interfaces();
+            reflector.Methods();
+            reflector.Runtimemethod("Met");
+        }
+    }
+}
